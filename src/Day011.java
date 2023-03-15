1bb8996ea8403b1d81fafa475ae4618e8bde44d0
Vidushika0316 <dasanayakasmvt.19@uom.lk>
2023-03-15 21:55:20 +0530

    completed day011

---
+++ src/Day011.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+public class Day011 {
+    public int searchInsert(int[] nums, int target) {
+
+        int a= Arrays.binarySearch(nums,target);
+        if(a>=0) return a;
+        else return Math.abs(a)-1;
+    }
+}
+//Search Insert Position
+
+//    Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+//
+//        You must write an algorithm with O(log n) runtime complexity.
+//
+//
+//
+//        Example 1:
+//
+//        Input: nums = [1,3,5,6], target = 5
+//        Output: 2
+//        Example 2:
+//
+//        Input: nums = [1,3,5,6], target = 2
+//        Output: 1
+//        Example 3:
+//
+//        Input: nums = [1,3,5,6], target = 7
+//        Output: 4
