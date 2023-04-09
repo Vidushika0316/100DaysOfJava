9fb4e023ddebe7204bd074d783be4a44c5393e2d
Vidushika0316 <dasanayakasmvt.19@uom.lk>
2023-04-09 12:17:55 +0530

    completed day030

--- src/Day030.java
+++ src/Day030.java
@@ -29,7 +29,6 @@ public class Day030 {
 }
 
 //Student Attendance Record I
-
 //    You are given a string s representing an attendance record for a student where each character signifies whether the student was absent, late, or present on that day. The record only contains the following three characters:
 //
 //        'A': Absent.
